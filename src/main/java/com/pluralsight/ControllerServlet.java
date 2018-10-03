66c838cd90281891a0d65ea92d4415fc1cf4ad9a
mjudge4 <33074714+mjudge4@users.noreply.github.com>
2018-10-03 15:03:17 +0100

    -am
    Help

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -65,6 +65,10 @@ public class ControllerServlet extends HttpServlet {
 			case "/edit":
 				showEditForm(request, response);
 				break;
+			case "/update":
+				updateBook(request, response);
+				break;
+    
         default:
 				   listBooks(request, response);
            break;
@@ -134,6 +138,19 @@ public class ControllerServlet extends HttpServlet {
 		RequestDispatcher reqDispatcher = request.getRequestDispatcher("/BookForm.jsp");
 		request.setAttribute("book", book);
 		reqDispatcher.forward(request, response);
+	}
+	
+	private void updateBook(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		int id = Integer.parseInt(request.getParameter("id"));
+		String title = request.getParameter("booktitle");
+		String author = request.getParameter("bookauthor");
+		String price = request.getParameter("bookprice");
+		
+		Book book = new Book(title, author, Float.parseFloat(price));
+		bookDAO.updateBook(book);
+		response.sendRedirect("list");
+		
+		
 	}
 
 }
